0a7b523dbcf9de621e86bf4baaa108b814a1c81c
willymateo <wjmateo@espol.edu.ec>
2022-01-16 09:39:59 -0500

    Scrapping para la página web Liga de Gamers y creación de csv implementada

---
+++ Extractores/ExtractorLiga.rb
@@ -0,0 +1,26 @@
+class ExtractorLiga
+
+  def extraerJuegos(url)
+    CSV.open('csv/ligagamers.csv','wb') do |csv|
+      csv << %w[top titulo genero plataforma fecha]
+    end
+    pagina = URI.open(url)
+    contenido = pagina.read
+    parsed = Nokogiri::HTML(contenido)
+
+    for i in (1..100).to_a.reverse
+      contenedorTitulo = parsed.css("div[class='article--body']").css("h2:nth-of-type(#{i})")
+      contenedor = parsed.css("div[class='article--body']").css("ul:nth-of-type(#{i})")
+      contenedor.each do |datos|
+        top = contenedorTitulo.inner_text.split(".")[0]
+        titulo = contenedorTitulo.inner_text.split(".")[1]
+        genero = datos.css("li:nth-of-type(3)").inner_text.split(':')[1..-1].join(' ').strip.split(",")[0]
+        plataforma = datos.css("li:nth-of-type(2)").inner_text.split(':')[1..-1].join(' ').strip.split(",")[0]
+        fecha = datos.css("li:first-of-type").inner_text.split(':')[1..-1].join(' ').strip
+        juego = Juegos.new(top,titulo,genero,plataforma,fecha)
+        juego.toString()
+        juego.registrar3()
+      end 
+    end
+  end
+end
